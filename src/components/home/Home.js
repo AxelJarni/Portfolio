f40fdbcff3834e605bbd0132506fa41e61889222
Axel Jarnigon <axel.jarnigon@gmail.com>
2021-05-05 13:48:41 +0200

    added animation on home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,16 @@
+import Typical from 'react-typical'
+
 function Home () {
     return(
         <div id='home'>
             <section className='homeTitle d-flex flex-column align-items-center justify-content-center'>
-                <h2>Developpeur Web et Web Mobile</h2>
-                <p>En apprentissage continuel</p>
+                <h2>Axel Jarnigon</h2>
+                <Typical
+                    steps={['Je suis développeur Web', 1500, 'Je suis curieux', 1500, 'Je suis en apprentissage permanent', 1500, 'Je suis disponible', 5000]}
+                    loop={Infinity}
+                    wrapper="p"
+                    className='typical'
+                />
             </section>
         </div>
     );
