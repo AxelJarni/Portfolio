b3f8f4f2044f69e1ca5435fe7c9d4c60fab21d74
Axel JARNIGON <axel.jarnigon@gmail.com>
2021-05-08 19:59:23 +0200

    version for review

--- src/components/contact/infocontact/InfoContact.js
+++ src/components/contact/infocontact/InfoContact.js
@@ -4,14 +4,15 @@ function InfoContact() {
             <li className="contactList">
                 <a href="tel:+33620127580">
                     <i className="fa fa-phone m-3"></i>
+                    <span>+33 6 20 12 75 80</span>
                 </a>
-                +33 6 20 12 75 80
+                
             </li>
             <li className='contactList'>
                 <a href='mailto:axel.jarnigon@gmail.com'>
-                    <i className="fa fa-envelope m-3"></i><span>axel.jarnigon@gmail.com</span>
+                    <i className="fa fa-envelope m-3"></i>
+                    <span>axel.jarnigon@gmail.com</span>
                 </a>
-                
             </li>
         </ul>
     );
